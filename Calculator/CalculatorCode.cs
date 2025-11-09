9bcc49841ba257978afa75a37f69680c22e70584
Bohdan Naumenko <131257541+setygur@users.noreply.github.com>
2025-11-09 10:58:47 +0100

    Initial commit + CalculatorCode

---
+++ Calculator/CalculatorCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator;
+
+public class CalculatorCode
+{
+    private double A { get; }
+    private double B { get; }
+    private char Operation { get; }
+
+    public CalculatorCode(double a, double b, char operation)
+    {
+        A = a;
+        B = b;
+        Operation = operation;
+    }
+
+    public double Execute()
+    {
+        return Operation switch
+        {
+            '+' => A + B,
+            '-' => A - B,
+            '*' => A * B,
+            '/' => B != 0 ? A / B : throw new DivideByZeroException("Cannot divide by zero."),
+            _   => throw new InvalidOperationException($"Invalid operation: {Operation}")
+        };
+    }
+}
---
+++ Calculator/CalculatorTests.cs
@@ -0,0 +1,6 @@
+﻿namespace Calculator;
+
+public class CalculatorTests
+{
+    
+}
