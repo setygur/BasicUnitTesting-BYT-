6fa736f68a043329ebd1d79d9b4295660fad2d67
setygur <131257541+setygur@users.noreply.github.com>
2025-11-09 12:31:29 +0100

    Tests

--- Calculator/CalculatorTests.cs
+++ Calculator/CalculatorTests.cs
@@ -1,6 +1,49 @@
-﻿namespace Calculator;
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator;
 
 public class CalculatorTests
 {
-    
+    [Test]
+    public void TestAddition()
+    {
+        var calc = new CalculatorCode(5, 3, '+');
+        Assert.That(calc.Execute(), Is.EqualTo(8));
+    }
+
+    [Test]
+    public void TestSubtraction()
+    {
+        var calc = new CalculatorCode(10, 4, '-');
+        Assert.That(calc.Execute(), Is.EqualTo(6));
+    }
+
+    [Test]
+    public void TestMultiplication()
+    {
+        var calc = new CalculatorCode(6, 7, '*');
+        Assert.That(calc.Execute(), Is.EqualTo(42));
+    }
+
+    [Test]
+    public void TestDivision()
+    {
+        var calc = new CalculatorCode(20, 5, '/');
+        Assert.That(calc.Execute(), Is.EqualTo(4));
+    }
+
+    [Test]
+    public void TestDivisionByZero()
+    {
+        var calc = new CalculatorCode(10, 0, '/');
+        Assert.Throws<DivideByZeroException>(() => calc.Execute());
+    }
+
+    [Test]
+    public void TestInvalidOperation()
+    {
+        var calc = new CalculatorCode(2, 3, '%');
+        Assert.Throws<InvalidOperationException>(() => calc.Execute());
+    }
 }
--- Shapes/Shapes_Tests.cs
+++ Shapes/Shapes_Tests.cs
@@ -20,6 +20,34 @@ namespace Tut2_s20123_Tests
         }
 
         //todo : Complete the remaning tests here
+        
+        private readonly IShape cylinder = new Cylinder(3, 7);
+
+        [Test]
+        public void TestCylinderCalculateArea()
+        {
+            Assert.That(cylinder.CalculateArea(), Is.EqualTo(188.496).Within(0.001));
+        }
+
+        [Test]
+        public void TestCylinderCalculateVolume()
+        {
+            Assert.That(cylinder.CalculateVolume(), Is.EqualTo(197.920).Within(0.001));
+        }
+        
+        private readonly IShape rectangle = new Rectangle(4, 8);
+
+        [Test]
+        public void TestRectangleCalculateArea()
+        {
+            Assert.That(rectangle.CalculateArea(), Is.EqualTo(32.000).Within(0.001));
+        }
+
+        [Test]
+        public void TestRectangleCalculateVolume()
+        {
+            Assert.That(rectangle.CalculateVolume(), Is.EqualTo(0.000).Within(0.001));
+        }
 
     }
 }
